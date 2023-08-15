c95136638e9edaf9271c9efdeeb0537e7c31366c
javascriptrocker2104 <117441918+javascriptrocker2104@users.noreply.github.com>
2023-08-15 12:54:47 +0300

    Update tables.sql

--- clickhouse/ddl/tables.sql
+++ clickhouse/ddl/tables.sql
@@ -1,52 +1,50 @@
-CREATE TABLE IF NOT EXISTS shops(
-shop_id Int32,
-shop_name String
-);
-ENGINE = MergeTree()
-PRIMARY KEY(shop_id);
-
-CREATE TABLE IF NOT EXISTS products(
-product_id Int32,
-product_name String,
-price Decimal32(2)
-);
+CREATE TABLE IF NOT EXISTS product
+( product_id Int32
+, product_name String
+, price Decimal32(2)
+)
 ENGINE = MergeTree()
 PRIMARY KEY(product_id);
 
-CREATE TABLE IF NOT EXISTS plan(
-product_id Int32,
-shop_id Int32,
-plan_cnt Int32,
-plan_date DATE
-);
+CREATE TABLE IF NOT EXISTS shops
+( shop_id Int32
+, shop_name String
+)
+ENGINE = MergeTree()
+PRIMARY KEY(shop_id);
+
+CREATE TABLE IF NOT EXISTS plan
+( product_id Int32
+, shop_id Int32
+, plan_cnt Int32
+, plan_date Date
+)
 ENGINE = MergeTree()
 ORDER BY product_id;
 
-CREATE TABLE IF NOT EXISTS shop_dns(
-product_id Int32,
-shop_id Int32,
-sales_cnt Int32,
-date DATE
-);
+CREATE TABLE IF NOT EXISTS shop_dns
+( shop_id Int32
+, product_id Int32
+, date Date
+, sales_cnt Int32
+)
 ENGINE = MergeTree()
 ORDER BY product_id;
 
-CREATE TABLE IF NOT EXISTS shop_mvideo(
-product_id Int32,
-shop_id Int32,
-sales_cnt Int32,
-date DATE
-);
-
+CREATE TABLE IF NOT EXISTS shop_mvideo
+( shop_id Int32
+, product_id Int32
+, date Date
+, sales_cnt Int32
+)
 ENGINE = MergeTree()
 ORDER BY product_id;
 
-CREATE TABLE IF NOT EXISTS shop_sitilink(
-product_id Int32,
-shop_id Int32,
-sales_cnt Int32,
-date DATE
-);
-
+CREATE TABLE IF NOT EXISTS shop_sitilink
+( shop_id Int32
+, product_id Int32
+, date Date
+, sales_cnt Int32
+)
 ENGINE = MergeTree()
 ORDER BY product_id;
